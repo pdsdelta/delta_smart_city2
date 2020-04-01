3de09e74fa0065d537fdc33815d4f121a3a88167
yacineredjechta <mohamed.redjechta@etu.u-pec.fr>
2020-04-01 17:42:48 +0200

    Update ConnectionFileReader.java
    test

--- smart-city-connecionPool/connectionPool/ConnectionFileReader.java
+++ smart-city-connecionPool/connectionPool/ConnectionFileReader.java
@@ -26,22 +26,27 @@ public class ConnectionFileReader {
 			//Pour le jar
 			//input = new FileInputStream("delta_smart_city2"+ File.separator +"Ressources" + File.separator + "bdd.properties");
 			
-			//Pour éclipse
-			input = new FileInputStream("Ressources" + File.separator + "bdd.properties");
+			//Pour ï¿½clipse
+			//input = new FileInputStream("Ressources" + File.separator + "bdd.properties");
 			
 			// load a properties file
-			pr.load(input);
+			//pr.load(input);
 
 			// get the property value and print it out
-			this.driver = pr.getProperty("bdd.driver");
-			this.connectionUrl =pr.getProperty("bdd.url");
-			this.user = pr.getProperty("bdd.username");
-			this.password = pr.getProperty("bdd.password");
+			
+			//this.driver = pr.getProperty("bdd.driver");
+			this.driver = pr.getProperty("org.postgresql.Driver");
+			//this.connectionUrl =pr.getProperty("bdd.url");
+			this.connectionUrl = "jdbc:postgresql://172.31.249.20:5432/delta-city-bd";
+			//this.user = pr.getProperty("bdd.username");
+			this.user = pr.getProperty("delta-city");
+			//this.password = pr.getProperty("bdd.password");
+			this.password = pr.getProperty("toto");
 			
 
-		} catch (final IOException ex) {
+		}/*catch (final IOException ex) {
 			ex.printStackTrace();
-		} finally {
+		}*/finally {
 			if (input != null) {
 				try {
 					input.close();
