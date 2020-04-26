e57aec2daf391b37699e6440035c96a34b28de70
JulienCombes <61044802+JulienCombes@users.noreply.github.com>
2020-04-26 12:09:50 +0200

    Update historic

--- smart-city-commons/Historique/Historic.java
+++ smart-city-commons/Historique/Historic.java
@@ -1,5 +1,39 @@
 package Historique;
+import java.sql.Date;
 
 public class Historic {
+	int idHistoric;
+	Date dateHistoric;
+	int indiceReleve;
 
+	public int getId() {
+		return idHistoric;
+	}
+	
+	public void setId(int id) {
+		this.idHistoric = id;
+	}
+
+	public Date getDate() {
+		return dateHistoric;
+	}
+	public void setDate(Date date) {
+		this.dateHistoric = date;
+	}
+	
+	public int getIndice() {
+		return indiceReleve;
+	}
+	public void setIndice(int indiceATMO) {
+		this.indiceReleve = indiceATMO;
+	}
+	
+	public void gethistodistrict(int idHistoric, Date dateHistoric, int indiceReleve){
+		String quartier = ;
+		System.out.println("L'historique "+idHistoric+" datant du "+dateHistoric+ "a relevé l'indice ATMO "+indiceReleve+ " dans le quartier "+quartier+" présente dans notre ville");
+		}
+
+	public void gethistocity(int idHistoric, Date dateHistoric, int indiceReleve){
+		System.out.println("L'historique "+idHistoric+" datant du "+dateHistoric+ "a relevé l'indice ATMO "+indiceReleve+ " dans notre ville ");
+	}
 }
