404acfefd92e1348c2f04701f9cc94163cf62eb7
Donfack Anaelle <anaellejeagni@gmail.com>
2020-05-18 00:57:16 +0100

    update gestion_borne.crud

--- smart-city-functionality/gestion_borne/crud/DAO.java
+++ smart-city-functionality/gestion_borne/crud/DAO.java
@@ -2,23 +2,26 @@ package gestion_borne.crud;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import city.city;
 import connectionPool.DataSource;
+import functionality_server.functionalityServer;
 import server.CityServer;
 
 public abstract class DAO<T> {
-	protected CityServer server;
+	protected functionalityServer server;
 	protected Connection connect;
 	protected DataSource connection;
-	public DAO(CityServer server) throws ClassNotFoundException, SQLException {
-		this.server= server;
+	public DAO(functionalityServer server2) throws ClassNotFoundException, SQLException {
+		this.server= server2;
 		this.connection= new DataSource();
 	    this.connect = connection.getConnection();
 	}
-	public abstract boolean create (T obj, String nameCity) throws SQLException;
+	public abstract boolean create (T obj) throws SQLException;
 	public abstract boolean delete(T obj);
 	public abstract boolean update (T obj);
 	public abstract T find(int id);
+	public abstract List<T> getAll() throws SQLException;
 
 }
