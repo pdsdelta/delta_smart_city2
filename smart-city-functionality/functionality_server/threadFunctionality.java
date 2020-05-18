cce68df31bed291bedb27660e8265b4f3153880c
Donfack Anaelle <anaellejeagni@gmail.com>
2020-05-18 01:10:05 +0100

    update ThreadFonctionnality

--- smart-city-functionality/functionality_server/threadFunctionality.java
+++ smart-city-functionality/functionality_server/threadFunctionality.java
@@ -104,6 +104,16 @@ class threadFunctionality extends Thread {
 				query = s.getGlobalCarbonne(date);
 				this.response = query;
 			}
+			
+			//Action concernant la simulation de circulation de voiture
+			if(operationType.equals("entrer")) {
+				System.out.println("Une voiture veut entrer");
+				this.response="Une voiture veut entrer";
+			}else if(operationType.equals("sortir")) {
+				System.out.println("Une voiture veut sortir");
+				this.response= "Une voiture veut sortir";
+
+			}
 			// Requ�te concernant les d�tecteurs de v�hicules
 			if (operationType.equals("CREATE_SENSOR")) {
 
