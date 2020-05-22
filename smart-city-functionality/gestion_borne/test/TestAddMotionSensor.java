1a4816b9284c659228d410a0b17f0fb106f6d068
Donfack Anaelle <anaellejeagni@gmail.com>
2020-05-22 11:56:17 +0100

    testMotionSennsor

---
+++ smart-city-functionality/gestion_borne/test/TestAddMotionSensor.java
@@ -0,0 +1,97 @@
+package gestion_borne.test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import borne.Terminal;
+import functionality_server.functionalityServer;
+import gestion_borne.crud.MotionSensorDAO;
+import gestion_borne.crud.TerminalDAO;
+import motionSensor.MotionSensor;
+
+public class TestAddMotionSensor {
+	static MotionSensorDAO data;
+	static functionalityServer server;
+	public TestAddMotionSensor() {}
+	public void constructObject() throws ClassNotFoundException, SQLException {
+		JSONArray request = readScenario();
+	
+        //Iterate over employee array
+        request.forEach( voit -> {
+			try {
+				parseVoitureObject( (JSONObject) voit );
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} );
+		
+		
+		
+		
+	}
+	private static void parseVoitureObject(JSONObject requete) throws ClassNotFoundException, SQLException 
+    {
+		
+        //Get employee object within list
+        JSONObject borne = (JSONObject) requete.get("request");
+         
+        //Get longitude 
+        long longi =  (Long) borne.get("longitude");    
+       
+         
+        //Get latitude
+        long lati =  (Long) borne.get("latitude");  
+        int longitude= (int)longi;
+        int latitude =(int)lati;
+        MotionSensor objet = new MotionSensor(longitude,latitude);
+        data= new MotionSensorDAO(server);
+		data.create(objet);
+       
+         
+        
+    }
+	public JSONArray readScenario() {
+		//JSON parser object to parse read file
+		JSONParser jsonParser = new JSONParser();
+		JSONArray terminal = null;
+		try (FileReader reader = new FileReader("E:\\AddTerminal.json"))
+		{
+			//Read JSON file
+			Object obj = jsonParser.parse(reader);
+			terminal = (JSONArray) obj;
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return terminal;
+
+	}
+	public static void main (String args[]) {
+		TestAddMotionSensor  test = new TestAddMotionSensor();
+	     try {
+			test.constructObject();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+
+}
---
+++ smart-city-functionality/gestion_borne/test/TestFindMotionSensor.java
@@ -0,0 +1,95 @@
+package gestion_borne.test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import borne.Terminal;
+import functionality_server.functionalityServer;
+import gestion_borne.crud.MotionSensorDAO;
+import gestion_borne.crud.TerminalDAO;
+import motionSensor.MotionSensor;
+
+public class TestFindMotionSensor {
+	static MotionSensorDAO data;
+	static functionalityServer server;
+	public TestFindMotionSensor() {}
+	public void constructObject() throws ClassNotFoundException, SQLException {
+		JSONArray request = readScenario();
+
+		//Iterate over employee array
+		request.forEach( voit -> {
+			try {
+				parseVoitureObject( (JSONObject) voit );
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} );
+
+
+
+
+	}
+	private static void parseVoitureObject(JSONObject requete) throws ClassNotFoundException, SQLException 
+	{
+
+		//Get employee object within list
+		JSONObject borne = (JSONObject) requete.get("request");
+
+		//Get longitude 
+		long longi =  (Long) borne.get("numero");    
+
+
+		int numero= (int)longi;
+
+		MotionSensor t;
+		data= new MotionSensorDAO(server);
+		t= data.find(numero);
+		System.out.println(t);
+
+
+	}
+	public JSONArray readScenario() {
+		//JSON parser object to parse read file
+		JSONParser jsonParser = new JSONParser();
+		JSONArray terminal = null;
+		try (FileReader reader = new FileReader("E:\\DeleteTerminal.json"))
+		{
+			//Read JSON file
+			Object obj = jsonParser.parse(reader);
+			terminal = (JSONArray) obj;
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return terminal;
+
+	}
+	public static void main (String args[]) {
+		TestFindMotionSensor test = new TestFindMotionSensor();
+		try {
+			test.constructObject();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
