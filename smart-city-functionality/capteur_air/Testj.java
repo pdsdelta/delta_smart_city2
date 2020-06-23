adb55befd0054ef592a3381eb27f91859beacbeb
JulienCombes <61044802+JulienCombes@users.noreply.github.com>
2020-06-23 23:57:58 +0200

    Update testcapteur

--- smart-city-functionality/capteur_air/Testj.java
+++ smart-city-functionality/capteur_air/Testj.java
@@ -9,6 +9,7 @@ import java.net.UnknownHostException;
 
 import org.json.JSONException;
 
+import CapteurAir.CapteurAir;
 import district.District;
 
 public class Testj {
@@ -29,6 +30,30 @@ public class Testj {
 		String response = in.readLine();
 		System.out.println("***** Résultat ******\n");
 		return response;
+	}
+	
+	CapteurAir util1 = new CapteurAir();
+	public String addcapteurj(String a, String b, String c, String d, String e) throws UnknownHostException, IOException, JSONException {
+		int idcapteur =Integer.parseInt(a); 
+		String datereleve = b;
+		int indiceatmo = Integer.parseInt(c);
+		String namecapteur = d;
+		int intervalle = Integer.parseInt(e);
+	
+	String json= "";
+
+	util1.setId(idcapteur);
+	util1.setName(datereleve);
+	util1.setIndice(indiceatmo);
+	util1.setName(namecapteur);
+	util1.setIntervalle(intervalle);
+	
+	json  ="{request:{ operation_type: ADDCAPTEUR, target: capteurair , idcapteur: "+util1.getId() + 
+			", datereleve: "+ util1.getDate() + ", indiceatmo : "+ util1.getIndice() + 
+			", namecapteur : " + util1.getName() + ", intervalle : " + util1.getIntervalle() +"}} " ;
+	this.startConnection("172.31.249.22", 2400,json);
+	System.out.println("Nous allons enregistrer un par un les capteurs");
+	return (String) json;
 	}
 	
 	District util = new District();
